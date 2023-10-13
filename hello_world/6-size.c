ac7034d1d230e97416680140735965508101aa9f
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-13 14:57:49 -0700

    Write a C program that prints the size of various types on the computer it is compiled and run on.

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
-*main- "C program that prints the size of various
-*       types on the computer it is compiled and run on.
+/**
+* main-  "C program that prints the size of various
+*         types on the computer it is compiled and run on.
 * 
-*Return:Always 0.
+* Return:Always 0.
 */
 int main(void)
 {
