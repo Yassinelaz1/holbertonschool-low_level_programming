c66803be2237f25e99f937d5947183de761f09ef
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-13 14:34:00 -0700

    5. Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,10 +4,12 @@
 *main - Entry point
 *
 *Return: 0
+*
 */
+
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");        
+printf("with proper grammar, but the outcome is a piece of art,\n");        
 return (0);
 }
 
