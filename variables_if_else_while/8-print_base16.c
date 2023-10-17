538ef0c2564951fc2672adf7d5a01428d7097579
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-17 15:00:21 +0100

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,15 +7,16 @@ int main(void)
 {
 char alphabet;
 int n;
-for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
-{
-putchar(alphabet);
-}
-
 for (n = 0; n < 10; n++)
 {
 printf("%d", n);
 }
+for (alphabet = 'a'; alphabet <= 'f' ; alphabet++)
+{
+putchar(alphabet);
+}
+
+
 putchar('\n');
 return (0);
 }
