726e65d4beca6b0b94a9c87a20157a82ab5fc70d
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-17 14:25:10 +0100

    6. Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
-* main-print 0123456789
+* main-print 0123456789 with ascci
 * Return:0
 */
 int main(void)
 {
 int n;
-for (n = 0; n < 10; n++)
+for (n = 48; n <= 57; n++)
 {
-printf("%d", n);
+putchar(n);
 }
 putchar('\n');
 return (0);
