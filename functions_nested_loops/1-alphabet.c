77983efa8a27ac9758cd5383b6d0f543db7186bd
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-18 10:18:42 +0100

    prints the alphabet, in lowercase,

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * main -  prints the alphabet, in lowercase,
- *
- * Return: Always 0.
- */
+* main -  prints the alphabet, in lowercase,
+*
+* Return: Always 0.
+*/
 void print_alphabet(void)
 {
 char alphabet;
