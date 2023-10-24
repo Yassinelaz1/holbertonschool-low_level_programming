34e4786a2117e21fdf51a62d7c4e01794b8613a3
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-24 09:28:28 +0100

    prints a string, followed by a new line

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+*_puts- prints a string, followed by a new line
+*Return: str
+*/
+void _puts(char *str)
+{
+    while(*str != '\0')
+    {
+        putchar(*str)
+        str++
+    }
+    putchar('\n')
+}
