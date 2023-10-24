4d06bcc3b251eb84b4f2155552ac396f908d6ee2
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-10-24 11:34:52 +0100

    prints a string, in reverse, followed by a new line.

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*print_rev- prints a string, in reverse, followed by a new line.
+*@s: char
+*Return: 0
+*/
+void print_rev(char *s)
+{
+while (*s = '\0')
+{
+putchar(*s);
+s++;
+}
+putchar('\n');
+}
