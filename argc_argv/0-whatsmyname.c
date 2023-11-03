0b962889ea3513bbd5b1bbdd60a5dcfe34376912
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-11-03 15:47:28 +0100

    a program that prints its name, followed by a new line.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@argc : int , number of args including name of the program
+ *@argv : array of strings ;
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
