f9a4bf326987f8816418ed04e8e1e36c98135304
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-11-17 16:22:59 +0100

    a function that searches for an integer.

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator-executes a function
+ * @array:pointer
+ * @size:int
+ * @action:function
+ * Return:void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+
+    if (size > 0 && array != NULL && cmp != NULL)
+        for (i = 0; i < size; i++)
+            if ((*cmp)(array[i]) == 1)
+                return (i);
+    return (-1);
+}
