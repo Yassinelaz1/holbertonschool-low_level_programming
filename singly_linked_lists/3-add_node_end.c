f4e0d325c5b7511f29c3da6759ef0b2a0f41641c
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-11-30 15:22:23 +0100

    function that frees a list_t list.

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,37 +7,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *node_end;
+	list_t *node_end;
 
-    node_end = malloc(sizeof(list_t));
+	node_end = malloc(sizeof(list_t));
 
-    if (node_end == NULL)
-    {
-        free(node_end);
-        return (NULL);
-    }
+	if (node_end == NULL)
+	{
+		free(node_end);
+		return (NULL);
+	}
 
-    node_end->str = strdup(str);
+	node_end->str = strdup(str);
 
-    if (node_end->str == NULL)
-    {
-        free(node_end);
-        return (NULL);
-    }
-    node_end->len = strlen(str);
-    node_end->next = NULL;
+	if (node_end->str == NULL)
+	{
+		free(node_end);
+		return (NULL);
+	}
+	node_end->len = strlen(str);
+	node_end->next = NULL;
 
-    if (*head == NULL)
-        *head = node_end;
-    else
-    {
-        list_t *last_node = *head;
+	if (*head == NULL)
+		*head = node_end;
+	else
+	{
+		list_t *last_node = *head;
 
-        while (last_node->next != NULL)
-        {
-            last_node = last_node->next;
-        }
-        last_node->next = node_end;
-    }
-    return (node_end);
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = node_end;
+	}
+	return (node_end);
 }
