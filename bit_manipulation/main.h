d439f94a7d8b3f1e23edb0bbf3a230bdc9dab0b3
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-03 17:12:02 +0100

    header file

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,11 +1,9 @@
 #ifndef Bi_manipulation
 #define Bit_manipulation
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
