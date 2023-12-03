9ae1a9eb27b7fa871c7cbe50db170e94e27b42cb
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-03 17:07:44 +0100

    returns the number of bits you would need to flip to get from one number to another.

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *flip_bits - sets the value of a bit to 0 at a given index.
+ *@n: origin decimal number.
+ *@m: destiny decimal number.
+ *Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bit = 0;
+	unsigned long int p = n ^ m;
+
+	while (p)
+	{
+		bit += p & 1;
+		p >>= 1;
+	}
+	return (bit);
+}
