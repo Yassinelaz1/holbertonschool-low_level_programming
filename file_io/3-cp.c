aa0a664522c5bd8aed27d53a1d69402082af7cda
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-09 18:22:18 +0100

    copies the content of a file to another file.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 	int file_from, file_to;
 	int READ = 1, WRITE;
-	mode_t MODE =S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	mode_t MODE = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
