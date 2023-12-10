3c4f6d21a566632eccb94c07edb0218ec52ab923
Yassinelaz1 <yassinelazhari524@gmail.com>
2023-12-10 20:38:14 +0100

    prints a hash table.

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	char c = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return(0);
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (c == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			c = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
